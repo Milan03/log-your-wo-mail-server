15463d3b9ccfb01a467f92eef53237933729efd9
Milan Sobat <milansobat03@gmail.com>
2021-07-01 11:40:20 -0400

    Added: another cors attempt

--- server.js
+++ server.js
@@ -9,13 +9,20 @@ const fs = require("fs");
 const app = express();
 
 //configure the Express middleware to accept CORS requests and parse request body into JSON
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// Set up CORS
+app.use(cors({
+    origin: true, // "true" will copy the domain of the request back
+                  // to the reply. If you need more control than this
+                  // use a function.
+
+    credentials: true, // This MUST be "true" if your endpoint is
+                       // authenticated via either a session cookie
+                       // or Authorization header. Otherwise the
+                       // browser will block the response.
+
+    methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+                                           // pre-flight OPTIONS requests
+}));
 app.use(bodyParser.json());
 
 //start application server on heroku assigned port or port 3000 if local
